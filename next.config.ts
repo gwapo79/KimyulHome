2b01868890d0a3c1c01353aa491abf9d67058504
gwapo79 <gwapo79@naver.com>
2025-12-23 22:47:33 +0900

    Update config for custom domain

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'xn--2i4bq4i58b81e.com', // 서초지율.com
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.xn--2i4bq4i58b81e.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.supabase.co'
+      }
+    ],
+  },
 };
 
 export default nextConfig;
