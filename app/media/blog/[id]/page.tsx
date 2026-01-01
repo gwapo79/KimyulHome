e7d28b9912898869da8cdaca6bc078456c469714
gwapo79 <gwapo79@naver.com>
2026-01-01 15:48:35 +0900

    fix: Add missing import for rehype-raw

--- app/media/blog/[id]/page.tsx
+++ app/media/blog/[id]/page.tsx
@@ -6,6 +6,7 @@ import { Metadata } from 'next';
 import { getTeamMemberByName } from '@/app/constants/team';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
+import rehypeRaw from 'rehype-raw';
 
 export const revalidate = 0;
 
